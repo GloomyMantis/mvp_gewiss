7a03621b0ebb4c2d70c129eb308304528e8f6f13
GloomyMantis <gloomymantis@gmail.com>
2026-04-03 10:22:04 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
 }
 module.exports = nextConfig
