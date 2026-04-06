e303718b1408d08af322d16e4a597f0a4880fa88
GloomyMantis <gloomymantis@gmail.com>
2026-04-06 13:03:54 +0300

    Delete mnt/user-data/outputs/files directory

--- mnt/user-data/outputs/files/dashboard-project-id-edit/page.tsx
+++
@@ -1,9 +0,0 @@
-import EditClient from './EditClient'
-
-export async function generateStaticParams() {
-  return [{ id: 'placeholder' }]
-}
-
-export default function EditProjectPage({ params }: { params: { id: string } }) {
-  return <EditClient id={params.id} />
-}
--- mnt/user-data/outputs/files/dashboard-project-id/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectDetailClient from './ProjectDetailClient'
-
-export async function generateStaticParams() {
-  return [{ id: 'placeholder' }]
-}
-
-export default function ProjectDetailPage({ params }: { params: { id: string } }) {
-  return <ProjectDetailClient id={params.id} />
-}
